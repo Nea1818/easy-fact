241e4f44fdb42b7e3d383f478f6699472bf57990
Nea18 <nanou718@hotmail.fr>
2019-08-12 00:53:43 +0200

    Ajustement homepage

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -4,12 +4,16 @@ const HomePage = props => {
   return (
     <div className='jumbotron'>
       <div className='container'>
-        <h1 className='display-3'>Gestion de factures</h1>
+        <h1 className='display-4'>Gestion de factures</h1>
         <p className='lead my-3'>
-          Cette API a été créee à partir d'une formation e-learning sur le site
-          Udemy. Les outils utilisés lors de ce tutoriel sont Symfony 4, API
-          Platform et React.
+          Cette API a été créée à partir d'une formation e-learning. Les outils
+          utilisés lors de ce tutoriel sont Symfony 4, API Platform et React.
+          Ci-dessous des identifiants de connexion d'un compte avec des données
+          enregistrées:
         </p>
+        <h5 className='mt-5 mb-2'>Email: api@gmail.com</h5>
+        <h5 className='mb-5'> Mot de passe: api</h5>
+        <p className='lead'>Bonne visite!</p>
         <hr className='my-4' />
         <p className='lead my-2'>
           <a
@@ -18,7 +22,7 @@ const HomePage = props => {
             target='_blank'
             role='button'
           >
-            En savoir plus
+            Accès à la formation e-learning
           </a>
         </p>
       </div>
